97bb39b2d17bd7c596978a1921c5fb23271481c2
Scott Nitro <scottnitro1@gmail.com>
2023-11-28 15:07:20 +0000

    third-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
-import { Fragment, useEffect, useState } from "react";
-import { styled } from "@mui/material/styles";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import {
@@ -12,61 +11,18 @@ import {
   timezoneAreaSuccess,
 } from "./redux/timezone/timezoneSlice";
 import Container from "@mui/material/Container";
-import MuiAccordion from "@mui/material/Accordion";
-import MuiAccordionSummary from "@mui/material/AccordionSummary";
-import MuiAccordionDetails from "@mui/material/AccordionDetails";
-import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import CircularProgress from "@mui/material/CircularProgress";
 import { format, utcToZonedTime, toDate } from "date-fns-tz";
 
-const Accordion = styled((props) => (
-  <MuiAccordion disableGutters elevation={0} square {...props} />
-))(({ theme }) => ({
-  border: `1px solid ${theme.palette.divider}`,
-  "&:not(:last-child)": {
-    borderBottom: 0,
-  },
-  "&:before": {
-    display: "none",
-  },
-}));
-
-const AccordionSummary = styled((props) => <MuiAccordionSummary {...props} />)(
-  ({ theme }) => ({
-    backgroundColor:
-      theme.palette.mode === "dark"
-        ? "rgba(255, 255, 255, .05)"
-        : "rgba(0, 0, 0, .03)",
-    flexDirection: "row-reverse",
-    "& .MuiAccordionSummary-content": {
-      marginLeft: theme.spacing(1),
-    },
-  })
-);
-
-const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-  padding: theme.spacing(2),
-  borderTop: "1px solid rgba(0, 0, 0, .125)",
-}));
+import { Disclosure } from "@headlessui/react";
+import { ChevronUpIcon } from "@heroicons/react/20/solid";
 
 function App() {
   const dispatch = useDispatch();
 
-  const {
-    allTImeZones,
-    allTimeZonesLoading,
-    timezoneAreaLoading,
-    timezoneArea,
-  } = useSelector((state) => state.timezone);
-
-  const [expanded, setExpanded] = useState(null);
-
-  const handleAccordionChange = (idx) => {
-    setExpanded((prevExpanded) => (prevExpanded === idx ? null : idx));
-  };
+  const { allTImeZones, timezoneArea } = useSelector((state) => state.timezone);
 
   useEffect(() => {
     const fetchTImezone = async () => {
@@ -120,99 +76,37 @@ function App() {
 
   return (
     <Container maxWidth="lg" className="App" sx={{ justifyContent: "center" }}>
-      <Box
-        sx={{
-          display: "flex",
-          width: "100%",
-          flexDirection: "column",
-          justifyContent: "center",
-          marginY: 5,
-          marginX: "auto",
-        }}
-      >
-        <Typography variant="h4" sx={{ padding: 2, color: "#154360" }}>
-          TimeZone Selection
-        </Typography>
-        {!allTimeZonesLoading && allTImeZones && allTImeZones.length > 0 ? (
-          allTImeZones.map((item, index) => {
-            if (item && item.length > 3) {
-              return (
-                <Fragment key={index}>
-                  <Accordion
-                    expanded={expanded === index ? true : false}
-                    onChange={() => handleAccordionChange(index)}
-                    sx={{ marginBottom: 0 }}
+      <Typography variant="h5" sx={{ padding: 2, color: "#4A148C" }}>
+        TimeZone Selection
+      </Typography>
+      <div className="mx-auto w-full rounded-2xl bg-white p-2">
+        {allTImeZones && allTImeZones.length > 0 ? (
+          allTImeZones.map((item, index) => (
+            <Disclosure key={index}>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button
+                    onClick={() => fetchArea(item)}
+                    className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-md font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75"
                   >
-                    <AccordionSummary
-                      onClick={() => fetchArea(item)}
-                      expandIcon={
-                        expanded === index ? (
-                          <RemoveIcon
-                            sx={{
-                              fontSize: "0.9rem",
-                              color: expanded === index ? "#fff" : "none",
-                            }}
-                          />
-                        ) : (
-                          <AddIcon
-                            sx={{
-                              fontSize: "0.9rem",
-                              color: expanded === index ? "none" : "#2C3E50",
-                            }}
-                          />
-                        )
-                      }
-                      sx={{
-                        backgroundColor:
-                          expanded === index ? "#85929E" : "#ccc",
-                        color: expanded === index ? "#fff" : "#154360",
-                      }}
-                    >
-                      <Typography>{item}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails
-                      sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        marginLeft: "auto",
-                        marginRight: "auto",
-                        width: "90%",
-                      }}
-                    >
-                      {!timezoneAreaLoading && timezoneArea ? (
-                        expanded === index &&
-                        item === timezoneArea?.timezone &&
-                        timezoneArea?.datetime && (
-                          <Box>
-                            <Typography>
-                              {formatDateTime(timezoneArea)}
-                            </Typography>
-                          </Box>
-                        )
-                      ) : (
-                        <Box
-                          sx={{
-                            display: "flex",
-                            width: "100%",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            flexDirection: "column",
-                            marginY: 5,
-                          }}
-                        >
-                          <CircularProgress disableShrink color="inherit" />
-                          <Typography sx={{ mt: 4 }}>
-                            Loading Data...Please Wait
-                          </Typography>
-                        </Box>
-                      )}
-                    </AccordionDetails>
-                  </Accordion>
-                </Fragment>
-              );
-            }
-            return null;
-          })
+                    <span>{item}</span>
+                    <ChevronUpIcon
+                      className={`${
+                        open ? "rotate-180 transform" : ""
+                      } h-5 w-5 text-purple-500`}
+                    />
+                  </Disclosure.Button>
+                  {open &&
+                    item === timezoneArea?.timezone &&
+                    timezoneArea?.datetime && (
+                      <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
+                        {formatDateTime(timezoneArea)}
+                      </Disclosure.Panel>
+                    )}
+                </>
+              )}
+            </Disclosure>
+          ))
         ) : (
           <Box
             sx={{
@@ -228,7 +122,7 @@ function App() {
             <Typography sx={{ mt: 4 }}>Loading...Please Wait</Typography>
           </Box>
         )}
-      </Box>
+      </div>
     </Container>
   );
 }
